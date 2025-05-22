b8e8e9f84ec79d81fd8feaeb9900d37cbcc89f10
Joseph <josep.munozs@duocuc.cl>
2025-05-22 11:48:01 -0400

    Cambio 1

--- ServicioSucursal/ServicioSucursal/src/main/java/com/Perfulandia/ServicioSucursal/repository/SucursalRepository.java
+++ ServicioSucursal/ServicioSucursal/src/main/java/com/Perfulandia/ServicioSucursal/repository/SucursalRepository.java
@@ -1,6 +1,8 @@
 package com.Perfulandia.ServicioSucursal.repository;
 
 import com.Perfulandia.ServicioSucursal.model.Sucursal;
+
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
